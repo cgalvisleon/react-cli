2c20beadfb3d40404bd40a4f613181f813710089
Cesar Galvis León <cgalvisleon@gmail.com>
2020-07-27 09:28:31 -0500

    Update

--- npm.md
+++ npm.md
@@ -1,3 +1,1 @@
-npm version 1.2.45 -f npm run build npm publish lib
-
 npm version 1.2.79 -f && npm run build && npm publish lib
--- utilities/locale/es-LA/index.d.ts
+++
@@ -1,4 +0,0 @@
-// This file is generated automatically by `scripts/build/typings.js`. Please, don't change it.
-
-import { esLA } from 'date-fns/locale';
-export default esLA;
